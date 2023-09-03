66980bbb87cba1b8d8eb16164cccf7195da151ea
adefelic <ad.defelice@live.com>
2023-09-03 10:03:44 -0400

    css cleaning. added nav animation

--- themes/sleigher/static/js/nav.js
+++ themes/sleigher/static/js/nav.js
@@ -1,1 +1,14 @@
-// on click toggle nav class
+if (document.readyState !== 'loading') {
+	addNavClickListener();
+} else {
+    document.addEventListener('DOMContentLoaded', function () {
+        addNavClickListener();
+    });
+}
+
+function addNavClickListener() {
+		document.querySelector("#nav-button").addEventListener("click", (event) => {
+		document.querySelector("#grid-container").classList.toggle("grid-container-nav-collapsed");
+	    // element.innerHTML += "Fuction executed! "
+	});
+}
