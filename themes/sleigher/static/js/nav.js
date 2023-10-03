fb494b111835a143e90f06345094a424d06a9fc4
adefelic <ad.defelice@live.com>
2023-10-03 12:09:19 -0400

    added togglable background effects

--- themes/sleigher/static/js/nav.js
+++ themes/sleigher/static/js/nav.js
@@ -1,16 +1,36 @@
 if (document.readyState !== 'loading') {
-	addNavClickListener();
+	addNavAccordionClickListener();
+	addThunderstormClickListener();
 } else {
 	document.addEventListener('DOMContentLoaded', function() {
-		addNavClickListener();
+		addNavAccordionClickListener();
+		addThunderstormClickListener();
 	});
 }
 
-function addNavClickListener() {
-	const navButton = document.querySelector("#nav-button");
-	navButton.addEventListener("click", (event) => {
+function addNavAccordionClickListener() {
+	const button = document.querySelector("#nav-button");
+	button.addEventListener("click", (event) => {
 		document.querySelector("#grid-container").classList.toggle("grid-container-nav-collapsed");
 		// todo find a way to toggle inner text
 		// navButton.innerHTML += "Fuction executed! "
+	});
+}
+
+function addThunderstormClickListener() {
+	const button = document.querySelector("#thunderstorm-button");
+	button.addEventListener("click", (event) => {
+		document.querySelector(".grid-item-main").classList.toggle("starry");
+		document.querySelector(".grid-item-main").classList.toggle("thunderstorm");
+		if (document.querySelector("#thunderstorm-button-image").style.display !== "none") {
+			document.querySelector("#thunderstorm-button-image").style.display = "none";
+		} else {
+			document.querySelector("#thunderstorm-button-image").style.display = "block";
+		}
+		if (document.querySelector("#thunderstorm-button-image").style.display !== "none") {
+			document.querySelector("#starry-button-image").style.display = "none";
+		} else {
+			document.querySelector("#starry-button-image").style.display = "block";
+		}
 	});
 }
